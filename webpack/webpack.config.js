3dcb22976f5bb7a4d045a3c34e1f98ec68cccc98
PNdunguMaina <ndunguwamaina@gmail.com>
2022-07-14 22:41:19 +0300

    update correct path in webpack/webpack.config.js file

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
