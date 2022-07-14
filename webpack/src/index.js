ec182c9c00c4f818c8373a9cbf9fb82ff93b2933
PNdunguMaina <ndunguwamaina@gmail.com>
2022-07-14 22:21:04 +0300

    create to do list structure

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,14 +1,47 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+const listedTasks = [
+  {
+    description: 'Attend Gym session',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'Watch a movie',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Attend Microverse session',
+    completed: false,
+    index: 3,
+  },
+  {
+    description: 'Go to business site',
+    completed: false,
+    index: 5,
+  },
+  {
+    description: 'Read a book',
+    completed: false,
+    index: 6,
+  },
+];
+
+const tasksContainer = document.getElementById('tasks-container');
+
+const listContainer = document.createElement ('ul');
+tasksContainer.className = 'tasks';
+tasksContainer.appendChild(listContainer);
+
+listedTasks.forEach(listedItems);
+
+function listedItems (task) {
+  const taskList = document.createElement('li');
+  taskList.className = 'task-list';
+  taskList.innerHTML = `
+  <input class="checkbox" type="checkbox" />
+  <label class="list-label">${task.description} <i class="fa-solid fa-ellipsis-vertical fa-icon"></i></label>
+  `
+  return listContainer.appendChild(taskList);  
+}
